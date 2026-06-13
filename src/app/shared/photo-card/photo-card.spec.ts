ae22cd94b6ad546b97243de8e92c7843f35b516b
Paweł Domaradzki <paweldomar@gmail.com>
2026-06-13 16:48:40 +0200

    Photo card component & UT

--- src/app/shared/photo-card/photo-card.spec.ts
+++ src/app/shared/photo-card/photo-card.spec.ts
@@ -6,17 +6,41 @@ describe('PhotoCard', () => {
   let component: PhotoCard;
   let fixture: ComponentFixture<PhotoCard>;
 
+  const photo1 = {
+    id: '1',
+    width: 200,
+    height: 300,
+    thumbnailUrl: 'https://picsum.photos/id/1/200/300',
+    fullSizeUrl: 'https://picsum.photos/id/1/1000/1400'
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PhotoCard],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PhotoCard);
+    fixture.componentRef.setInput('photo', photo1);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render photo thumbnail', () => {
+    const image = fixture.nativeElement.querySelector('img') as HTMLImageElement;
+
+    expect(image.src).toContain(photo1.thumbnailUrl);
+  });
+
+  it('should emit photoClick when clicked', () => {
+    const emitSpy = vi.spyOn(component.photoClicked, 'emit');
+    const button = fixture.nativeElement.querySelector('button') as HTMLButtonElement;
+
+    button.click();
+
+    expect(emitSpy).toHaveBeenCalledWith(photo1);
   });
 });
--- src/app/shared/photo-card/photo-card.ts
+++ src/app/shared/photo-card/photo-card.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, input, output} from '@angular/core';
+import {Photo} from '../../da/models/photo.model';
+import {MatCard} from '@angular/material/card';
 
 @Component({
   selector: 'app-photo-card',
-  imports: [],
+  imports: [
+    MatCard
+  ],
   templateUrl: './photo-card.html',
   styleUrl: './photo-card.scss',
 })
-export class PhotoCard {}
+export class PhotoCard {
+  readonly photo = input.required<Photo>();
+  readonly photoClicked = output<Photo>();
+
+  onPhotoClicked(): void {
+    this.photoClicked.emit(this.photo());
+  }
+}
