162dfe04a7b1b37840db479051bdbb1c0f1adc9d
Paweł Domaradzki <paweldomar@gmail.com>
2026-06-14 00:31:45 +0200

    Update app.spec.ts

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,59 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { Header } from './layout/header/header';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  template: '',
+})
+class HeaderStub {}
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let component: App;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
+      imports: [App, HeaderStub],
+    })
+      .overrideComponent(App, {
+        remove: {
+          imports: [Header],
+        },
+        add: {
+          imports: [HeaderStub],
+        },
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, image-library');
+  it('should render the header component', () => {
+    const header = fixture.debugElement.query(By.css('app-header'));
+
+    expect(header).toBeTruthy();
+  });
+
+  it('should render the main application container', () => {
+    const main = fixture.debugElement.query(By.css('main.app-main'));
+
+    expect(main).toBeTruthy();
+  });
+
+  it('should render router outlet inside the main application container', () => {
+    const main = fixture.debugElement.query(By.css('main.app-main'));
+    const routerOutlet = main.query(By.directive(RouterOutlet));
+
+    expect(routerOutlet).toBeTruthy();
   });
 });
