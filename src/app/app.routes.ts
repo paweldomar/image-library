e52cbc9fbb406a1af4cf349cccf71ce922269a29
Paweł Domaradzki <paweldomar@gmail.com>
2026-06-13 13:09:27 +0200

    Routing & config
    add EsLint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Image Library',
+    loadComponent: () => import('./features/photos/photos-page/photos-page').then(m => m.PhotosPage),
+  },
+  {
+    path: 'favorites',
+    title: 'Favorites',
+    loadComponent: () => import('./features/favorites/favorites-page/favorites-page').then(m => m.FavoritesPage),
+  },
+  {
+    path: 'photos/:id',
+    title: 'Photo details',
+    loadComponent: () => import('./features/photo-details/photo-details-page/photo-details-page').then(m => m.PhotoDetailsPage),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
